81b82c9d62a47d5444d103ed2ea7f288a46f39d5
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-05-23 17:19:57 -0400

    Agreganfo por composer los vendors para usar api de google

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,30 +23,33 @@ class ComposerAutoloaderInit7185cf98ba0fda6c5ee1a072d0cb0034
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit7185cf98ba0fda6c5ee1a072d0cb0034', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+        $map = require __DIR__ . '/autoload_namespaces.php';
+        foreach ($map as $namespace => $path) {
+            $loader->set($namespace, $path);
+        }
 
-            call_user_func(\Composer\Autoload\ComposerStaticInit7185cf98ba0fda6c5ee1a072d0cb0034::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
+        $map = require __DIR__ . '/autoload_psr4.php';
+        foreach ($map as $namespace => $path) {
+            $loader->setPsr4($namespace, $path);
+        }
 
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
+        $classMap = require __DIR__ . '/autoload_classmap.php';
+        if ($classMap) {
+            $loader->addClassMap($classMap);
         }
 
         $loader->register(true);
 
+        $includeFiles = require __DIR__ . '/autoload_files.php';
+        foreach ($includeFiles as $file) {
+            composerRequire7185cf98ba0fda6c5ee1a072d0cb0034($file);
+        }
+
         return $loader;
     }
+}
+
+function composerRequire7185cf98ba0fda6c5ee1a072d0cb0034($file)
+{
+    require $file;
 }
